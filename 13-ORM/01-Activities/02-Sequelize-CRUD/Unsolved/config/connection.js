c7af037863a30b969b519c8743db1625d6a04291
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-07-27 21:44:56 -0500

    completed activities

--- 13-ORM/01-Activities/02-Sequelize-CRUD/Unsolved/config/connection.js
+++ 13-ORM/01-Activities/02-Sequelize-CRUD/Unsolved/config/connection.js
@@ -4,12 +4,12 @@ const Sequelize = require('sequelize');
 // Creates mySQL connection using Sequelize
 // Include your MySQL user/password information
 const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize('todolist', 'root', '', {
-      host: 'localhost',
-      port: 3306,
-      dialect: 'mysql'
-    });
+	? new Sequelize(process.env.JAWSDB_URL)
+	: new Sequelize('todolist', 'root', 'Fr334m3!', {
+			host: 'localhost',
+			port: 3306,
+			dialect: 'mysql',
+	  });
 
 // Exports the connection for other files to use
 module.exports = sequelize;
--- 13-ORM/01-Activities/02-Sequelize-CRUD/Unsolved/routes/api-routes.js
+++ 13-ORM/01-Activities/02-Sequelize-CRUD/Unsolved/routes/api-routes.js
@@ -8,38 +8,54 @@ const Todo = require('../models/todo');
 
 // GET route for getting all of the todos
 router.get('/api/todos', (req, res) => {
-  // Retrieve all of the todos from the database
-  // Return the todos as JSON
-  //
-  // YOUR CODE HERE
-  //
+	Todo.findAll({})
+		.then((dbTodo) => res.json(dbTodo))
+		.catch((err) => res.json(err));
 });
 
 // POST route for saving a new todo
 router.post('/api/todos', (req, res) => {
-  // Create a new todo with the data in 'req.body'
-  // Return the new todo as JSON
-  //
-  // YOUR CODE HERE
-  //
+	Todo.create(req.body)
+		.then((dbTodo) => res.json(dbTodo))
+		.catch((err) => res.json(err));
 });
 
 // DELETE route for deleting a todo
 router.delete('/api/todos/:id', (req, res) => {
-  // Delete a todo using the id from 'req.params.id'
-  // Return the result as JSON
-  //
-  // YOUR CODE HERE
-  //
+	Todo.destroy({
+		where: {
+			id: req.params.id,
+		},
+	})
+		.then((dbTodo) => {
+			if (!dbTodo) {
+				res.status(404).json({ message: 'No user found with this id' });
+				return;
+			}
+			res.json(dbTodo);
+		})
+		.then((doTodo) => res.json(dbTodo))
+		.catch((err) => res.json(err));
 });
 
 // PUT route for updating a todo
 router.put('/api/todos/:id', (req, res) => {
-  // Update a todo with the data in 'req.body'
-  // Return the result as JSON
-  //
-  // YOUR CODE HERE
-  //
+	Todo.update(req.body, {
+		where: {
+			id: req.params.id,
+		},
+	})
+		.then((dbTodo) => {
+			if (!dbTodo[0]) {
+				res.status(404).json({ message: 'No user found with this id' });
+				return;
+			}
+			res.json(dbTodo);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
 });
 
 module.exports = router;
