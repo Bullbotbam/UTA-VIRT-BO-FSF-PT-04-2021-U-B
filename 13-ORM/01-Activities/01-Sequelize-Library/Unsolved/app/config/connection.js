670f193b9eb9bb129aa3d3cbd14fb773e6ed7b6d
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-07-27 21:44:31 -0500

    completed activities

--- 13-ORM/01-Activities/01-Sequelize-Library/Unsolved/app/config/connection.js
+++ 13-ORM/01-Activities/01-Sequelize-Library/Unsolved/app/config/connection.js
@@ -4,17 +4,17 @@ const Sequelize = require('sequelize');
 // Creates mySQL connection using Sequelize
 // Include your MySQL user/password information
 const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize('sequelize_library', 'root', '', {
-      host: 'localhost',
-      port: 3306,
-      dialect: 'mysql',
-      pool: {
-        max: 5,
-        min: 0,
-        idle: 10000
-      }
-    });
+	? new Sequelize(process.env.JAWSDB_URL)
+	: new Sequelize('sequelize_library', 'root', 'Fr334m3!', {
+			host: 'localhost',
+			port: 3306,
+			dialect: 'mysql',
+			pool: {
+				max: 5,
+				min: 0,
+				idle: 10000,
+			},
+	  });
 
 // Exports the connection for other files to use
 module.exports = sequelize;
--- 13-ORM/01-Activities/01-Sequelize-Library/Unsolved/app/routes/api-routes.js
+++ 13-ORM/01-Activities/01-Sequelize-Library/Unsolved/app/routes/api-routes.js
@@ -9,37 +9,57 @@ const Book = require('../models/book');
 
 // Get all books
 router.get('/books', (req, res) => {
-  //
-  // YOUR CODE HERE
-  //
+	Book.findAll()
+		.then((dbBooks) => res.json(dbBooks))
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
 });
 
 // Get all books of a specific genre
 router.get('/genres/:genre', (req, res) => {
-  //
-  // YOUR CODE HERE
-  //
+	Book.findAll({
+		where: {
+			genre: req.body.genre,
+		},
+	}).then((response) => {
+		res.json(response).catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
+	});
 });
 
 // Get all books from a specific author
 router.get('/authors/:author', (req, res) => {
-  //
-  // YOUR CODE HERE
-  //
+	Book.findAll({
+		where: {
+			authors: req.patams.author,
+		},
+	});
 });
 
 // Get all "long" books (books 150 pages or more)
 router.get('/books/length/long', (req, res) => {
-  //
-  // YOUR CODE HERE
-  //
+	Book.findAll({
+		where: {
+			pages: {
+				[Op.gte]: 350,
+			},
+		},
+	});
 });
 
 // Get all "short" books (books 150 pages or less)
 router.get('/books/length/short', (req, res) => {
-  //
-  // YOUR CODE HERE
-  //
+	Book.findAll({
+		where: {
+			pages: {
+				[Op.lt]: 350,
+			},
+		},
+	});
 });
 
 module.exports = router;
